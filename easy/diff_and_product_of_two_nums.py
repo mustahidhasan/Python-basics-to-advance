46476b4717ab50024f197d46ea654045f6a1cb16
Mustahid Hasan <46712349+mustahidhasan@users.noreply.github.com>
2026-07-09 11:26:04 +0600

    save2

--- easy/diff_and_product_of_two_nums.py
+++ easy/diff_and_product_of_two_nums.py
@@ -1,9 +1,19 @@
 # 1. using map funciton a b gets the 2 input iteratively and doese the performation with a-b and a*b
 
-# 1. a, b = (map(int, ["10", "20"].split()))
-# 2. a,b = [10,20].split()
-# 3. a = 10
-#    b = 20
+# User Input
+#    |
+#    v
+# "10 20"              input()
+#    |
+#    v
+# ["10", "20"]         split()
+#    |
+#    v
+# 10, 20               map(int, ...)
+#    |
+#    v
+# a = 10
+# b = 20               unpacking
 
 a, b = (map(int, input().split()))
 print(a-b)
