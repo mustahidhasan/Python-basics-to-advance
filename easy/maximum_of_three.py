9d24424b46770d912d1d94391ab4977ca8351871
Mustahid Hasan <46712349+mustahidhasan@users.noreply.github.com>
2026-07-09 14:03:33 +0600

    save

--- easy/maximum_of_three.py
+++ easy/maximum_of_three.py
@@ -1,7 +1,5 @@
 # print the maxium among the 3 numbers
 
 a, b, c = map(int, input().split())
-if a > b and a > c:
-    print(a)
-else:
-    print(b if b > c else c)
+if a > b and a > c:print(a)
+else:print(b if b > c else c)
