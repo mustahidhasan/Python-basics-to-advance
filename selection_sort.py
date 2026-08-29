2218f510340578c7c1bcec6ef653737739f81f46
Mustahid Hasan <46712349+mustahidhasan@users.noreply.github.com>
2026-08-29 23:11:23 +0600

    feat: Implement selection sort skeleton for ascending and descending order

---
+++ selection_sort.py
@@ -0,0 +1,23 @@
+# perform the selection sort both ascending and descending
+
+#Ascending:
+# 1. 
+# 2. 
+# 3. 
+# 4.
+def ascending_sort_selection(values, n):
+    pass
+#Descending:
+# 1. 
+# 2. 
+# 3. 
+# 4.
+def descending_sort_selection(values, n):
+    pass
+
+
+if __name__ == "__main__":
+    values = [12, 4, 5, 9, 0, 2, 100]
+    n = len(values) # for the breaking logic of the loops 
+    print("Ascending Sort with Selection Sort", ascending_sort_selection(values, n))
+    print("Descending Sort with Selection Sort", descending_sort_selection(values, n))
