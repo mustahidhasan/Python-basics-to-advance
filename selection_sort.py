8556787e90fa712ea5d6adf842f8d5c6edbcfffd
Mustahid Hasan <46712349+mustahidhasan@users.noreply.github.com>
2026-08-31 00:11:31 +0600

    feat: Add basic implementation for ascending and descending selection sort functions

--- selection_sort.py
+++ selection_sort.py
@@ -1,5 +1,24 @@
 # perform the selection sort both ascending and descending
+#BASIC SOLUTION:
+#Ascending:
+# 1. 
+# 2. 
+# 3. 
+# 4.
+def ascending_sort_selection_basic(values, n):
+    pass
+#Descending:
+# 1. 
+# 2. 
+# 3. 
+# 4.
+def descending_sort_selection_basic(values, n):
+    pass
 
+
+
+
+# Optimized Solution:
 #Ascending:
 # 1. 
 # 2. 
@@ -19,5 +38,7 @@ def descending_sort_selection(values, n):
 if __name__ == "__main__":
     values = [12, 4, 5, 9, 0, 2, 100]
     n = len(values) # for the breaking logic of the loops 
-    print("Ascending Sort with Selection Sort", ascending_sort_selection(values, n))
-    print("Descending Sort with Selection Sort", descending_sort_selection(values, n))
+    print("Ascending Sort with Selection Sort(BASIC)", ascending_sort_selection_basic(values, n))
+    # print("Descending Sort with Selection Sort(BASIC)", descending_sort_selection_basic(values, n))
+    # print("Ascending Sort with Selection Sort(OPTIMIZED)", ascending_sort_selection(values, n))
+    # print("Descending Sort with Selection Sort(OPTIMIZED)", descending_sort_selection(values, n))
