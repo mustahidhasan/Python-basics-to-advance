d6e9a64185e65ac8212666b9633a6d3826149170
Mustahid Hasan <46712349+mustahidhasan@users.noreply.github.com>
2026-09-01 00:47:39 +0600

    feat: Add initial implementation for ascending and descending insertion sort functions

---
+++ insertion_sort.py
@@ -0,0 +1,26 @@
+# perform the insertion sort here with the array, assending and descending both
+
+
+#ASSENDING:
+# 1. 
+# 2. 
+# 3. 
+# 4.
+def insertion_sort_assending(values, n):
+    pass
+
+
+#Descending:
+# 1. 
+# 2. 
+# 3. 
+# 4. 
+
+def insertion_sort_descending(values, n):
+    pass
+
+if __name__ == "__main__":
+    values = [5, 2, 0, 19, 100, 55]
+    n = len(values)
+    print("Insertion Sort Ascending: ", insertion_sort_assending(values, n))
+    print("Insertion Sort Descending: ", insertion_sort_descending(values, n))
