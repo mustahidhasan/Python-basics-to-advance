fa49196fa7550f6c5a7aa374e3dee454c09f1442
Mustahid Hasan <46712349+mustahidhasan@users.noreply.github.com>
2026-09-03 22:57:32 +0600

    feat: Implement basic and optimized insertion sort functions for ascending and descending order

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,12 +1,12 @@
 # perform the insertion sort here with the array, assending and descending both
 
-
+# Basic Pop replace method
 #ASSENDING:
 # 1. 
 # 2. 
 # 3. 
 # 4.
-def insertion_sort_assending(values, n):
+def insertion_sort_assending_basic(values, n):
     pass
 
 
@@ -16,11 +16,32 @@ def insertion_sort_assending(values, n):
 # 3. 
 # 4. 
 
-def insertion_sort_descending(values, n):
+def insertion_sort_descending_basic(values, n):
+    pass
+
+# Optimised way: Copy past mehthod.
+#ASSENDING:
+# 1. 
+# 2. 
+# 3. 
+# 4.
+def insertion_sort_assending_optimized(values, n):
+    pass
+
+
+#Descending:
+# 1. 
+# 2. 
+# 3. 
+# 4. 
+
+def insertion_sort_descending_optimized(values, n):
     pass
 
 if __name__ == "__main__":
     values = [5, 2, 0, 19, 100, 55]
     n = len(values)
-    print("Insertion Sort Ascending: ", insertion_sort_assending(values, n))
-    print("Insertion Sort Descending: ", insertion_sort_descending(values, n))
+    print("Insertion Sort Ascending: ", insertion_sort_assending_basic(values, n))
+    print("Insertion Sort Descending: ", insertion_sort_descending_basic(values, n))
+    print("Insertion Sort Ascending: ", insertion_sort_assending_optimized(values, n))
+    print("Insertion Sort Descending: ", insertion_sort_descending_optimized(values, n))
