65889eca89974e93f823cbc5fab99936cb5044b5
Mustahid Hasan <46712349+mustahidhasan@users.noreply.github.com>
2026-09-09 21:47:15 +0600

    feat: Add function to calculate the length of the last word in a string with examples

---
+++ pblm/length_of_last_string.py
@@ -0,0 +1,29 @@
+# Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+# A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+# Example 1:
+
+# Input: s = "Hello World"
+# Output: 5
+# Explanation: The last word is "World" with length 5.
+# Example 2:
+
+# Input: s = "   fly me   to   the moon  "
+# Output: 4
+# Explanation: The last word is "moon" with length 4.
+# Example 3:
+
+# Input: s = "luffy is still joyboy"
+# Output: 6
+# Explanation: The last word is "joyboy" with length 6.
+
+def lenght_last_string(s):
+    words = list(s.split())
+    return len(words[-1])
+
+if __name__ == "__main__":
+    s = "   fly me   to   the moon  "
+    print(lenght_last_string(s))
